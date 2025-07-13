11fb8768f2886bc067c2e76d965a0e412bb92ed0
John Voight <jvoight@gmail.com>
2025-07-12 17:30:19 -0700

    Ups, now we really check nonequivalence of groups.

--- all-nonselfdual-groups.m
+++ all-nonselfdual-groups.m
@@ -1,14 +1,15 @@
 ell := 2;
 GL4ell := GL(4,GF(ell));
+// J := Matrix(GF(ell),[[0,0,1,0],[0,0,0,0],[-1,0,0,0],[0,0,0,0]]);
 J := Matrix(GF(ell),[[0,1,0,0],[-1,0,0,0],[0,0,0,0],[0,0,0,0]]);
 V := KMatrixSpace(GF(ell),2,2);
-gs := [V | g : g in Generators(ConformalSymplecticGroup(2,ell))] cat [V!Id(ConformalSymplecticGroup(2,ell))];
+gs := [V | g : g in Generators(GSp(2,ell))] cat [V!Id(GSp(2,ell))];
 hs := [V | h : h in Generators(GL(2,ell))] cat [V!Id(GL(2,ell))];
 z := V![[0,0],[0,0]];
 ms := [V | m : m in Basis(V)] cat [z];
 Ggen := [GL4ell | BlockMatrix([[g,z],[m,h]]) : g in gs, h in hs, m in ms];
 G := sub<GL4ell | Ggen>;
-assert #G eq #ConformalSymplecticGroup(2,ell)*#GL(2,ell)*#V;
+assert #G eq #GSp(2,ell)*#GL(2,ell)*#V;
  
 sim := function(g);
   gjg := Transpose(g)*J*g;
@@ -20,23 +21,40 @@ end function;
 _ := [sim(g) : g in G];
 
 Gsubs := Subgroups(G);
-Gout_notequiv_sub := [* *];  // those where the subgroup is not even isomorphic
-Gout_notequiv := [* *];  // those without an equivalence to the twisted contragredient
+Gout_notequiv_isom := [* *];  // those where the subgroup is not even isomorphic
+Gout_notequiv_conj := [* *];  // those without an equivalence of subgroups to the twisted contragredient
+Gout_notequiv_realz := [* *];  // the group is conjugate to the dual but the reps are not equivalent
 Gout_notequiv_all := [* *];
 for i := 1 to #Gsubs do
-  Hrec := Gsubs[i];
   print i;
+  Hrec := Gsubs[i];
   H := Hrec`subgroup;
   Hdual := sub<GL4ell | [Transpose(h^(-1))*sim(h) : h in H]>;
   if not IsIsomorphic(H,Hdual) then
-    print "!!!! not even an isomorphic group", Hrec;
-    Append(~Gout_notequiv_sub, Hrec);
+    print "!! not even an isomorphic group", Hrec;
+    Append(~Gout_notequiv_isom, Hrec);
     Append(~Gout_notequiv_all, Hrec);
     continue;
   end if;
-  if not IsConjugate(GL4ell,H,Hdual) then
-    print "!!!!", Hrec;
-    Append(~Gout_notequiv, Hrec);
+  bl, nu := IsConjugate(GL4ell,H,Hdual);
+  if not bl then
+    print "!!! not conjugate in GL_4(FF_ell)", Hrec;
+    Append(~Gout_notequiv_conj, Hrec);
     Append(~Gout_notequiv_all, Hrec);
+    continue;
   end if;
+
+  // OK, now H^nu = Hdual, so we'd have to get the dual from something in the normalizer.
+  N := Normalizer(GL4ell,Hdual);
+  for h in H do
+    hdual := Transpose(h^(-1))*sim(h);
+    N := [n : n in N | h^(nu*n) eq hdual];
+    if N eq [] then
+      print "!!!! not conjugate for realz", Hrec;
+      Append(~Gout_notequiv_realz, Hrec);
+      Append(~Gout_notequiv_all, Hrec);
+      break;
+    end if;
+  end for;
+  // assert &and[&and[h^(nu*n) eq Transpose(h^(-1))*sim(h) : h in H] : n in N];
 end for;
